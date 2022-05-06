fa9e487cd876a42b6a9b01aa477cd4a68995e2fa
Alina130613 <92860068+Alina130613@users.noreply.github.com>
2022-05-06 09:54:19 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import toTopBtn from './modules/toTopBtn'
 import timerclock from './modules/timerclock'
 import calculatorCounter from './modules/calculatorCntr'
 import modal from './modules/modal'
-
+import formSender from './modules/formSender'
 
 toTopBtn()
 calculatorCounter()
 timerclock('10 may 2022')
 modal()
+
+const forms = document.querySelectorAll('form')
+forms.forEach((form, index) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        form.id = `form${index}`
+        
+        formSender({
+            formId: `form${index}`,
+            someElem: [
+                {
+                    type: 'block',
+                    id: 'calc-total'
+                }
+            ] 
+        })
+    })
+})
